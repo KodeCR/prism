38bbd6eb7331ba807dd35dd581542b420e877d54
KodeCR <37069053+KodeCR@users.noreply.github.com>
2026-01-28 21:58:42 +0100

    Clean-up install and update submodules

--- install.sh
+++ install.sh
@@ -24,22 +24,24 @@ brew install bazel buildifier
 
 # install colima, with support for cross-compilation
 brew install colima lima-additional-guestagents qemu
-eval "$(brew shellenv)"
+# eval "$(brew shellenv)"
+
 # install docker
 brew install docker docker-buildx docker-compose
-INS=",\n\t\"cliPluginsExtraDirs\": [\n\t\t\"$HOMEBREW_PREFIX/lib/docker/cli-plugins\"\n\t]"
-sed -i "" "s|\"currentContext\": \"colima\"|\"currentContext\": \"colima\"${INS}|" ~/.docker/config.json
-mkdir -p ~/.docker/certs.d && cp certs/* ~/.docker/certs.d
+# INS=",\n\t\"cliPluginsExtraDirs\": [\n\t\t\"$HOMEBREW_PREFIX/lib/docker/cli-plugins\"\n\t]"
+# sed -i "" "s|\"currentContext\": \"colima\"|\"currentContext\": \"colima\"${INS}|" ~/.docker/config.json
+# mkdir -p ~/.docker/certs.d && cp certs/* ~/.docker/certs.d
 
 # if using container registry then build multi-platform
 # if multi-platform and macos arm then vz and rosetta
 # if multi-platform and macos intel then qemu
-$ROSETTA=
+ROSETTA=''
 if [[ $(arch) == 'arm64' ]]; then
     softwareupdate --install-rosetta
-    $ROSETTA='--vz-rosetta'
+    ROSETTA='--vz-rosetta'
 fi
 
 # enable multi-platform builds
-colima start --memory 4 --vm-type=vz $ROSETTA && colima stop # --cpus 4 --disk 128
+colima start --memory 4 --vm-type=vz $ROSETTA && colima stop
+# --cpus 4 --disk 128
 # sed -i "" "s|docker: {}|docker:\n  features:\n    containerd-snapshotter: true|" ~/.colima/default/colima.yaml # default since v29
